5e33c8b9aeaeac98ab728e536af4b836456982c3
AnneMarie Caballero <agcaballero@google.com>
2020-07-29 22:21:13 +0000

    Use Class.forName() to get PerspectiveAPIKey class if present

--- backstory/src/main/java/com/google/sps/servlets/PerspectiveServlet.java
+++ backstory/src/main/java/com/google/sps/servlets/PerspectiveServlet.java
@@ -16,12 +16,13 @@ package com.google.sps.servlets;
 
 import au.com.origma.perspectiveapi.v1alpha1.PerspectiveAPI;
 import com.google.gson.Gson;
-import com.google.sps.data.PerspectiveAPIKey;
 import com.google.sps.data.PerspectiveAnalysis;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Map;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -31,18 +32,27 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that filters text using the Perspective API. */
 @WebServlet("/perspective")
 public final class PerspectiveServlet extends HttpServlet {
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // prepare response to return JSON and set up a GSON object
     response.setContentType("application/json;");
     Gson gson = new Gson();
+    
+    String apiKey = "foo";
 
-    String apiKey = PerspectiveAPIKey.getKey();
-
-    if (apiKey.equals("foo")) {
-      // custom response if key couldn't be retrieved
+    try {
+      // fetch the PerspectiveAPIKey class if it's there
+      Class<?> keyClass = Class.forName("com.google.sps.data.PerspectiveAPIKey");
+      // create a method getKey that takes no parameters
+      Method getKey = keyClass.getMethod("getKey", null);
+      // invoke this static method (first null means it's static 
+      // & second null means it does not need arguments) & stores result
+      apiKey = (String) getKey.invoke(null, null);
+    } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+      // if any errors were thrown for looking for api key, send this error message back to JS servlet
       String errorMessage = "Could not retrieve the Perspective API.";
-      response.getWriter().println(gson.toJson(errorMessage));
+      System.out.println(errorMessage);
       return;
     }
 
