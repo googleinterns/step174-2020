e2c2982e7a3cb432f5fc1691deb8029ef20cdbfb
AnneMarie Caballero <agcaballero@google.com>
2020-08-05 02:32:44 +0000

    Make files pretty

--- backstory/src/main/java/com/google/sps/perspective/PerspectiveAPIFactoryImpl.java
+++ backstory/src/main/java/com/google/sps/perspective/PerspectiveAPIFactoryImpl.java
@@ -19,14 +19,13 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
- * Uses local Java file containing API key to create 
+ * Uses local Java file containing API key to create
  * new instances of PerspectiveAPI class.
  */
 public class PerspectiveAPIFactoryImpl implements PerspectiveAPIFactory {
-
   /** The name of the file with the API key (including its package) */
   private static final String API_KEY_FILE = "com.google.sps.data.perspective.PerspectiveAPIKey";
-  
+
   /* The name of the method within the API_KEY_FILE to get the key */
   private static final String GET_KEY_METHOD_NAME = "getKey";
 
@@ -35,24 +34,26 @@ public class PerspectiveAPIFactoryImpl implements PerspectiveAPIFactory {
 
   /**
    * Constructs a factory by finding and setting the api key for this factory.
-   * 
+   *
    * @throws ClassNotFoundException if it cannot find the file with the api key (PerspectiveAPIKey)
    * @throws NoSuchMethodException if said file doesn't have the method to get the key (getKey)
-   * @throws IllegalAccessException if class called from doesn't have proper access to the method to get the key (getKey)
-   * @throws InvocationTargetException if the method to get the key (getKey()) itself throws an exception
+   * @throws IllegalAccessException if class called from doesn't have proper access to the method to
+   *     get the key (getKey)
+   * @throws InvocationTargetException if the method to get the key (getKey()) itself throws an
+   *     exception
    */
   public PerspectiveAPIFactoryImpl() throws ClassNotFoundException, NoSuchMethodException,
-      IllegalAccessException, InvocationTargetException {
+                                            IllegalAccessException, InvocationTargetException {
     // fetch the PerspectiveAPIKey class if it's there
     Class<?> keyClass = Class.forName(API_KEY_FILE);
-    
-    // create a method getKey that takes no parameters 
+
+    // create a method getKey that takes no parameters
     // (which is what null param of getMethod signifies)
     Method getKey = keyClass.getMethod(GET_KEY_METHOD_NAME, null);
 
-    // invoke static method getKey() (first null means it's static 
+    // invoke static method getKey() (first null means it's static
     // & second null means it does not need arguments) & stores result in apiKey
-    apiKey = (String) getKey.invoke(null, null); 
+    apiKey = (String) getKey.invoke(null, null);
   }
 
   /**
