600b7e9f796baed9ddb3e72121ed5c4ef16ffcd9
Uday <ukalra@ucsd.edu>
2020-08-07 06:32:18 +0000

    Add functionality to GPT2 demo page. Allow page to post servlet which calls generateText()

--- backstory/src/demo/webapp/gpt2.js
+++ backstory/src/demo/webapp/gpt2.js
@@ -1,5 +1,4 @@
 // Copyright 2020 Google LLC
-//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -17,3 +16,79 @@
  */
 
 // DISPLAY STORY
+
+async function displayStory() {
+  /* eslint-disable no-unused-vars */
+  const input = document.getElementById('prompt').value;
+
+  if (input === '' || input === null) {
+    alert('You need to enter a value');
+    return;
+  }
+
+  console.log(input);
+
+  // get display
+  const display = document.getElementById('story-display');
+  display.appendChild(formatResponse('Loading...'));
+  // grab data and get its text version (it is sent as JSON)
+  const response = await fetch('/gpt2', {
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: `{text: ${input}}`,
+  });
+
+  const ok = response.ok;  // checks for server error
+  const data = await response.text();
+
+  display.innerHTML = data;
+
+  // parse the JSON into an object
+  const jsonObject = JSON.parse(data);
+
+  console.log(jsonObject);
+
+  // properly format and display either the error message or the results
+  if (!ok) {
+    display.innerHTML = formatErrorMessage(jsonObject);
+  } else {
+    if (display.firstChild) display.firstChild.remove();
+
+    display.appendChild(formatResponse(jsonObject));
+  }
+}
+
+/**
+ * Returns HTML formatting (simple paragraph tags) for error message
+ *
+ * @param {string} message - the error message to be shown
+ * @return {string} - HTML formatting for error message
+ */
+function formatErrorMessage(message) {
+  return `<p>${message}</p>`;
+}
+
+/**
+ * Adds HTML formatting to JSON response to display properly on page
+ * Constructs a table to display the attributes & display the
+ * decision from the JSON as "Approved" or "Disapproved" above table.
+ * Returns an element which is a div containing all this formatting.
+ *
+ * @param {boolean} - decision the decision on whether it's appropriate
+ * @param {object} - attributes a map of attribute scores and types
+ * @return {object} - Element that displays attribute scores & decision
+ */
+function formatResponse(text) {
+  const container = document.createElement('div');
+
+  const generatedText = document.createElement('p');
+  generatedText.id = 'outputText';
+
+  generatedText.innerText = text;
+
+
+  // add the approval to the larger container
+  container.appendChild(generatedText);
+
+  return container;
+}
